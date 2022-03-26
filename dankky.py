168085615d0a06afe59558c1be844b851561a389
Black Rose <89435091+BlackRose-000@users.noreply.github.com>
2022-03-26 09:54:57 -0500

    Update 4.0.0
    Sorry for the late update I school had started again so i had to study.

--- dankky.py
+++ dankky.py
@@ -6,6 +6,7 @@ import pyautogui
 import os
 from ctypes import windll, Structure, c_long, byref
 from datetime import datetime
+import threading
 s=False
 
 clear = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
@@ -15,11 +16,10 @@ beg = True
 dig=True
 hunt =True
 fish=True
-crime=True
 search=True
 pm=True
 sell = True
-
+crime=True
 Setting = input('''
 
 ┏━━━┓╋╋┏┓╋┏┓
@@ -32,19 +32,22 @@ Setting = input('''
 ╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋╋┗━━┛
 
 Type the letters of the commands you want to remove then press 'Enter' you do not
-need to add any spaces if you don't want to remove any then press 'Enter' 
+need to add any spaces if you don't want to remove any then press 'Enter' or you can
+do 'N' if you do not want anything that might make you die
+
+DISCLAIMER - Regradless of what the bot does there is still a chance for you to die.
 
 b ~ Beg
 d ~ Dig
 h ~ Hunt
 f ~ Fish
-c ~ Crime 
 s ~ Search
-p ~ Pm
+p ~ Pm 
+                                      Input: ''')
 
 
-''')
-
+Setting1 = input('Do you have a \'Tip Jar\' (y/n): ')
+tipjar=False
 if 'b' in Setting:
   beg = False
 if 'd' in Setting:
@@ -53,14 +56,27 @@ if 'h' in Setting:
   hunt= False
 if 'f' in Setting:
   fish = False
-if 'c' in Setting:
-  crime = False
 if 's' in Setting:
   search = False
 if 'p' in Setting:
   pm = False
-
-
+if 'N' in Setting:
+  search = False
+  crime = False
+if 'y' in Setting1:
+  tipjar=True
+else:
+  tipjar == False
+isint = True
+while (isint):
+    Setting2 =input("How many 'Pizzas' do you want to use : ")
+    try:
+        Setting2 =int(Setting2)
+        isint=False
+    except ValueError:
+        print("Type in a number.")
+        sleep(2)
+        clear()
 clear()
 print('''
   
@@ -98,7 +114,7 @@ def waste():
   presteige = "pls prestige"
   shop = "pls shop"
   meme = "pls meme"
-  waste = [profile,presteige,shop,meme]
+  waste = [profile,shop,meme]
   randomw = random.choice(waste)
   pyautogui.typewrite(randomw)  
 def wastee():
@@ -124,10 +140,27 @@ def waitS():
 
 current_time = datetime.now()
 Dank='\033[32m[Dank]\u001b[0m'
+x=0
+def pizza():
+  global Setting2,x
+  if Setting2 != x:
+    pyautogui.press("enter")
+    pyautogui.typewrite('pls use pizza') 
+    pyautogui.press("enter")
+    Setting2 -= 1
+    
 
+def tip():
+  pyautogui.press("enter")
+  pyautogui.typewrite('pls use tipjar') 
+  pyautogui.press("enter")
 
-
-while(s):
+while (s):
+  if tipjar== True:
+    threading.Timer(20, tip).start()
+  
+  threading.Timer(25, pizza).start()
+  
   if dig == True:
     pyautogui.typewrite("pls dig")
     waitS()
@@ -138,11 +171,21 @@ while(s):
     waitS()
     pyautogui.press('enter')
     print(f"[{current_time}] - {Dank} | Successfully sent commmand 'pls fish' ")
+    waitS()
+    pyautogui.moveTo(pos)
+    pyautogui.leftClick(pos)
+    pyautogui.move(0, 50 , 1 , pyautogui.easeInQuad) 
+    pyautogui.leftClick(pos)
   if hunt==True:
     pyautogui.typewrite("pls hunt")
     waitS()
     pyautogui.press('enter')
     print(f"[{current_time}] - {Dank} | Successfully sent commmand 'pls hunt' ")
+    waitS()
+    pyautogui.moveTo(pos)
+    pyautogui.leftClick(pos)
+    pyautogui.move(0, 50 , 1 , pyautogui.easeInQuad) 
+    pyautogui.leftClick(pos)
   if beg == True:
     pyautogui.typewrite("pls beg")
     waitS()
@@ -186,7 +229,7 @@ while(s):
   pyautogui.moveTo(pos)
   pyautogui.leftClick(pos)
   pyautogui.move(0, 50 , 1 , pyautogui.easeInQuad) 
-  print(f"[{current_time}] - [{Dank}] | Successfully sent commmand 'pls sell' ")
+  print(f"[{current_time}] - {Dank} | Successfully sent commmand 'pls sell' ")
   pyautogui.leftClick(pos)
   waste()
   waitS()
